e397507402bc3dae53fd9f91c1c07ec744d5ab08
MVBoshev19 <63718955+MVBoshev@users.noreply.github.com>
2022-02-06 20:31:33 +0200

    Fixed the main.cpp

---
+++ Code/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+    void Menu()
+    {
+        cout << "                                                  HISTORY SEARCHER\n";
+        cout << endl;
+        cout << "                                          Press 1 to search an event by date\n ";
+        cout << endl;
+        cout << "                                          Press 2 to search an event by name\n";
+        cout << endl;
+        cout << "                                          Press 3 to enter an event in the database\n";
+        cout << endl;
+        cout << "                                          Press 4 to exit the program\n";
+        cout << endl;
+
+    }
