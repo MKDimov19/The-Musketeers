0ab37ba376f628c1985c6f8fa91ccbf0542bc0a4
MVBoshev19 <63718955+MVBoshev@users.noreply.github.com>
2022-02-08 18:49:10 +0200

    Added 2 new functions

--- Code/main.cpp
+++ Code/main.cpp
@@ -27,3 +27,29 @@ void printList(Node* node)
 
     }
  
+
+
+
+void push(Node** head_ref, int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+void insertAfter(Node* prev_node, int new_data)
+{
+    if (prev_node == NULL)
+    {
+        cout << "The given previous node cannot be NULL";
+        return;
+    }
+
+    Node* new_node = new Node();
+
+    new_node->data = new_data;
+
+    new_node->next = prev_node->next;
+
+    prev_node->next = new_node;
+}
