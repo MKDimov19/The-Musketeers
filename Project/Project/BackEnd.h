62b3427341bf55f930b8247a4e9f875aa8585ee0
Kamen Darakliev <imradev19@codingburgas.bg>
2022-02-12 13:46:54 +0200

    Add gray code

--- Project/Project/BackEnd.h
+++ Project/Project/BackEnd.h
@@ -2,86 +2,183 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <sstream>
 using namespace std;
-struct Node {
-    string data;
-    struct Node* next;
+
+struct Event {
+	string year;
+	string person;
+	bool isWinForBulgaria;
+
+	void print() {
+		cout << "Year " << year << "; Related to " << person << "; " << (isWinForBulgaria ? "Bulgaria wins" : "Bulgaria loses") << endl;
+	}
 };
+struct Node {
+	string grayCodeId;
+	//string data;
+	Event data;
+	struct Node* next;
+};
+
+//this is not used for now but might be useful
+int stringToInt(string value) {
+	stringstream intValue(value);
+	int number = 0;
+	intValue >> number;
+
+	return number;
+}
+
+//convert the int to binary
+string intToBinary(int number) {
+	string result = "";
+	while (number!=0)
+	{
+		result = (number % 2 == 0 ? "0" : "1") + result;
+		number /= 2;
+	}
+	return result;
+}
+
+// Helper function to xor two characters
+char xor_c(char a, char b) { return (a == b) ? '0' : '1'; }
+
+// Helper function to flip the bit
+char flip(char c) { return (c == '0') ? '1' : '0'; }
+
+// function to convert binary string
+// to gray string
+string binaryToGray(string binary)
+{
+	string gray = "";
+
+	// MSB of gray code is same as binary code
+	gray += binary[0];
+
+	// Compute remaining bits, next bit is computed by
+	// doing XOR of previous and current in Binary
+	for (int i = 1; i < binary.length(); i++) {
+		// Concatenate XOR of previous bit
+		// with current bit
+		gray += xor_c(binary[i - 1], binary[i]);
+	}
+
+	return gray;
+}
+
+// function to convert gray code string
+// to binary string
+string grayToBinary(string gray)
+{
+	string binary = "";
+
+	// MSB of binary code is same as gray code
+	binary += gray[0];
+
+	// Compute remaining bits
+	for (int i = 1; i < gray.length(); i++) {
+		// If current bit is 0, concatenate
+		// previous bit
+		if (gray[i] == '0')
+			binary += binary[i - 1];
+
+		// Else, concatenate invert of
+		// previous bit
+		else
+			binary += flip(binary[i - 1]);
+	}
+
+	return binary;
+}
+
+int binaryToInt(string binary) {
+	return stoi(binary, 0, 2);
+}
+
+int nextId = 1;
+
 void printList(Node* node)
 {
-    while (node != NULL)
-    {
-        cout << " " << node->data;
-        node = node->next;
-    }
+	while (node != NULL)
+	{
+		int nodeID = binaryToInt(grayToBinary(node->grayCodeId));
+		cout << "#" << nodeID << ". ";
+		node->data.print();
+		node = node->next;
+	}
 }
-void push(Node** head_ref, string new_data)
+void push(Node** head_ref, Event new_data)
 {
-    Node* new_node = new Node();
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
+	Node* new_node = new Node();
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	new_node->grayCodeId = binaryToGray(intToBinary(nextId));
+	(*head_ref) = new_node;
+	nextId++;
 }
-void append(Node** head_ref, string new_data)
+void append(Node** head_ref, Event new_data)
 {
-    Node* new_node = new Node();
+	Node* new_node = new Node();
 
-    Node* last = *head_ref;
+	Node* last = *head_ref;
 
-    new_node->data = new_data;
+	new_node->data = new_data;
 
-    new_node->next = NULL;
+	new_node->next = NULL;
 
-    if (*head_ref == NULL)
-    {
-        *head_ref = new_node;
-        return;
-    }
+	new_node->grayCodeId = binaryToGray(intToBinary(nextId));
 
-    while (last->next != NULL)
-    {
-        last = last->next;
-    }
+	if (*head_ref == NULL)
+	{
+		*head_ref = new_node;
+		nextId++;
+		return;
+	}
 
-    last->next = new_node;
-    return;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new_node;
+	nextId++;
+	return;
 }
 bool search(Node* head, string x)
 {
-    Node* current = head;
-    while (current != NULL)
-    {
-        if (current->data == x)
-            return true;
-        current = current->next;
-    }
-    return false;
+	Node* current = head;
+	while (current != NULL)
+	{
+		if (current->data.year == x || current->data.person == x)
+			return true;
+		current = current->next;
+	}
+	return false;
 }
-void deleteNode(Node** head_ref, string key)
+void deleteNode(Node** head_ref, int id)
 {
+	Node* temp = *head_ref;
+	Node* prev = NULL;
 
-    Node* temp = *head_ref;
-    Node* prev = NULL;
+	if (temp != NULL && temp->grayCodeId == binaryToGray(intToBinary(id)))
+	{
+		*head_ref = temp->next;
+		delete temp;
+		return;
+	}
+	else
+	{
+		while (temp != NULL && temp->grayCodeId != binaryToGray(intToBinary(id)))
+		{
+			prev = temp;
+			temp = temp->next;
+		}
 
-    if (temp != NULL && temp->data == key)
-    {
-        *head_ref = temp->next;
-        delete temp;
-        return;
-    }
-    else
-    {
-        while (temp != NULL && temp->data != key)
-        {
-            prev = temp;
-            temp = temp->next;
-        }
+		if (temp == NULL)
+			return;
 
-        if (temp == NULL)
-            return;
-
-        prev->next = temp->next;
-
-        delete temp;
-    }
+		prev->next = temp->next;
+		delete temp;
+	}
 }
--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -6,147 +6,166 @@
 using namespace std;
 Node* showMenu1(Node* head)
 {
-    string year, person, win;
-    cout << endl;
-    cout << "         Enter the year: ";
-    cin >> year;
-    cout << "         Enter the person the event is connected with: ";
-    cin >> person;
-    cout << "         Does Bulgaria win the battle? ";
-    cin >> win;
-    push(&head, year);
-    push(&head, person);
-    push(&head, win);
-    return head;
+	Event data;
+	cout << endl;
+	cout << "         Enter the year: ";
+	cin >> data.year;
+	cout << "         Enter the person the event is connected with: ";
+	cin >> data.person;
+	cout << "         Does Bulgaria win the battle? ";
+	string word;
+	cin >> word;
+	if (word == "yes")
+	{
+		data.isWinForBulgaria = true;
+	}
+	else
+	{
+		data.isWinForBulgaria = false;
+	}
+
+	push(&head, data);
+	return head;
 }
 Node* showMenu2(Node* head)
 {
-    string year, person, win;
-    cout << endl;
-    cout << "         Enter the year: ";
-    cin >> year;
-    cout << "         Enter the person the event is connected with: ";
-    cin >> person;
-    cout << "         Does Bulgaria win the battle? ";
-    cin >> win;
-    append(&head, year);
-    append(&head, person);
-    append(&head, win);
-    return head;
+	Event data;
+	cout << endl;
+	cout << "         Enter the year: ";
+	cin >> data.year;
+	cout << "         Enter the person the event is connected with: ";
+	cin >> data.person;
+	cout << "         Does Bulgaria win the battle? ";
+	string word;
+	cin >> word;
+	if (word == "yes")
+	{
+		data.isWinForBulgaria = true;
+	}
+	else
+	{
+		data.isWinForBulgaria = false;
+	}
+	append(&head, data);
+	return head;
 }
-Node* showMenu3(Node* head)
+void showMenu3(Node* head)
 {
-    string element;
-    cout << endl;
-    cout << "         Search for a year or person: ";
-    cin >> element;
-    search(head, element) ? cout << "Yes, it's there." : cout << "No, it's not there.";
-    Sleep(1000);
-    return head;
+	string element;
+	cout << endl;
+	cout << "         Search for a year or person: ";
+	cin >> element;
+	search(head, element) ? cout << "Yes, it's there." : cout << "No, it's not there.";
+	//Sleep(1000);
+	cout << endl;
+	system("pause");
 }
 Node* showMenu4(Node* head)
 {
-    string element;
-    cout << endl;
-    cout << "         Delete an element: ";
-    cin >> element;
-    deleteNode(&head, element);
-    return head;
+	cout << endl;
+	cout << "         ID of the element to delete: ";
+	int id;
+	cin >> id;
+	deleteNode(&head, id);
+	return head;
 }
 
 int main()
 {
-    Node* head = NULL;
-    int Set[] = { 7,7,7,7,7,7 };
-    int counter = 1;
-    char key;
+	Node* head = NULL;
+	int Set[] = { 7,7,7,7,7,7 };
+	int counter = 1;
+	char key;
 
-    title();
+	title();
 
-    for (int i = 0;;)
-    {
+	for (int i = 0;;)
+	{
 
-        gotoxy(10, 8);
-        color(Set[0]);
-        cout << "1. Add a new event in the start of the list";
+		gotoxy(10, 8);
+		color(Set[0]);
+		cout << "1. Add a new event in the start of the list";
 
-        gotoxy(10, 10);
-        color(Set[1]);
-        cout << "2. Add a new event in the end of the list";
+		gotoxy(10, 10);
+		color(Set[1]);
+		cout << "2. Add a new event in the end of the list";
 
-        gotoxy(10, 12);
-        color(Set[2]);
-        cout << "3. Search for a new event";
+		gotoxy(10, 12);
+		color(Set[2]);
+		cout << "3. Search for a new event";
 
-        gotoxy(10, 14);
-        color(Set[3]);
-        cout << "4. Delete an event";
+		gotoxy(10, 14);
+		color(Set[3]);
+		cout << "4. Delete an event";
 
-        gotoxy(10, 16);
-        color(Set[4]);
-        cout << "6. Display the current list";
+		gotoxy(10, 16);
+		color(Set[4]);
+		cout << "6. Display the current list";
 
-        gotoxy(10, 18);
-        color(Set[5]);
-        cout << "7. Exit";
-        cout << endl;
+		gotoxy(10, 18);
+		color(Set[5]);
+		cout << "7. Exit";
+		cout << endl;
 
-        key = _getch();
+		key = _getch();
 
-        if (key == 72 && (counter >= 2 && counter <= 6))
-        {
-            counter--;
-        }
-        if (key == 80 && (counter >= 1 && counter <= 5))
-        {
-            counter++;
-        }
-        if (key == '\r')
-        {
-            if (counter == 1)
-            {
-                system("cls");
-                head = showMenu1(head);
-                system("cls");
-                title();
-            }
-            if (counter == 2)
-            {
-                system("cls");
-                head = showMenu2(head);
-                system("cls");
-                title();
-            }
-            if (counter == 3)
-            {
-                system("cls");
-                showMenu3(head);
-                system("cls");
-                title();
-            }
-            if (counter == 4)
-            {
-                system("cls");
-                head = showMenu4(head);
-                system("cls");
-                title();
-            }
-            if (counter == 5)
-            {
-                printList(head);
-            }
-            if (counter == 6)
-            {
-                return 0;
-            }
-        }
-        Set[0] = 7; Set[1] = 7; Set[2] = 7; Set[3] = 7;  Set[4] = 7; Set[5] = 7;
-        if (counter == 1) Set[0] = 12;
-        if (counter == 2) Set[1] = 12;
-        if (counter == 3) Set[2] = 12;
-        if (counter == 4) Set[3] = 12;
-        if (counter == 5) Set[4] = 12;
-        if (counter == 6) Set[5] = 12;
-    }
+		if (key == 72 && (counter >= 2 && counter <= 6))
+		{
+			counter--;
+		}
+		if (key == 80 && (counter >= 1 && counter <= 5))
+		{
+			counter++;
+		}
+		if (key == '\r')
+		{
+			if (counter == 1)
+			{
+				system("cls");
+				head = showMenu1(head);
+				system("cls");
+				title();
+			}
+			if (counter == 2)
+			{
+				system("cls");
+				head = showMenu2(head);
+				system("cls");
+				title();
+			}
+			if (counter == 3)
+			{
+				system("cls");
+				showMenu3(head);
+				system("cls");
+				title();
+			}
+			if (counter == 4)
+			{
+				system("cls");
+				head = showMenu4(head);
+				system("cls");
+				title();
+			}
+			if (counter == 5)
+			{
+				printList(head);
+				system("pause");
+				system("cls");
+				title();
+			}
+			if (counter == 6)
+			{
+				return 0;
+			}
+		}
+		Set[0] = 7; Set[1] = 7; Set[2] = 7; Set[3] = 7;  Set[4] = 7; Set[5] = 7;
+		if (counter == 1) Set[0] = 12;
+		if (counter == 2) Set[1] = 12;
+		if (counter == 3) Set[2] = 12;
+		if (counter == 4) Set[3] = 12;
+		if (counter == 5) Set[4] = 12;
+		if (counter == 6) Set[5] = 12;
+	}
 
 }
